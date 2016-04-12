fbded126d56f02ab2311eb5811bda1dca8c2eb8c
mitsuchie <fsakura01@gmail.com>
2016-04-12 23:19:43 +0900

    move neobundle to dein

---
+++ dein.bat
@@ -0,0 +1,1 @@
+git clone https://github.com/Shougo/dein.vim.git dein/repos/github.com/Shougo/dein.vim
--- neobundle.bat
+++
@@ -1,2 +0,0 @@
-mkdir %USERPROFILE%\vimfiles\bundle
-git clone https://github.com/Shougo/neobundle.vim %USERPROFILE%\vimfiles\bundle\neobundle.vim
